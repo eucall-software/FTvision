07f92a731f62b7e63351cd285341f76715a91d51
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2018-04-27 15:07:35 +0200

    Fixing language to German, fix colorscale and make it look nice.

--- FTvisionBW.py
+++ FTvisionBW.py
@@ -2,24 +2,26 @@
 Simply display the contents of the webcam with optional mirroring using OpenCV
 via the new Pythonic cv2 interface.  Press <esc> to quit.
 '''
+
+import cv2
 from cv2 import VideoCapture
 from cv2 import flip
-from cv2 import imread, imshow
+from cv2 import imread, imshow, imwrite
 from cv2 import waitKey
 from cv2 import destroyAllWindows
-import cv2
 
-from exfel_colormap import exfel_colormap_r as xfel
+from exfel_colormap import exfel_colormap as xfel
 import numpy as np
 import colorsys
 import time
+import subprocess
 import matplotlib
 from matplotlib.cm import ScalarMappable
 
 import pdb
 
-IMAGE_SIZE = [509,509,3]
-WINDOW_OFFSET = [500,00]
+IMAGE_SIZE = [640,480,3]
+WINDOW_OFFSET = [320,0]
 
 def makeGaussian(size,sigma_x, sigma_y, pointing):
     if sigma_x == 0.0:
@@ -40,47 +42,44 @@ def makeGaussian(size,sigma_x, sigma_y, pointing):
 def nothing(arg):
     pass
 
-def show_webcam():
+def show_webcam(camera_id=0):
 
-    cam = VideoCapture(0)
+    cv2.namedWindow('Kontrollraum')
+    cv2.namedWindow('XFEL Strahl')
+    cv2.namedWindow('Messobjekt')
+    cv2.namedWindow('Bestrahlung')
+    cv2.namedWindow('Streubild')
 
-    cv2.namedWindow('Control Room')
-    cv2.namedWindow('Beam')
-    cv2.namedWindow('Object')
-    cv2.namedWindow('Illumination')
-    cv2.namedWindow('Detector image')
+    cv2.resizeWindow('XFEL Strahl', IMAGE_SIZE[0], IMAGE_SIZE[1])
+    cv2.resizeWindow('Messobjekt', IMAGE_SIZE[0], IMAGE_SIZE[1])
+    cv2.resizeWindow('Bestrahlung', IMAGE_SIZE[0], IMAGE_SIZE[1])
+    cv2.resizeWindow('Streubild', IMAGE_SIZE[0], IMAGE_SIZE[1])
 
-    cv2.resizeWindow('Beam', IMAGE_SIZE[0], IMAGE_SIZE[1])
-    cv2.resizeWindow('Object', IMAGE_SIZE[0], IMAGE_SIZE[1])
-    cv2.resizeWindow('Illumination', IMAGE_SIZE[0], IMAGE_SIZE[1])
-    cv2.resizeWindow('Detector image', IMAGE_SIZE[0], IMAGE_SIZE[1])
+    cv2.moveWindow('Kontrollraum', 0, 0 )
+    cv2.moveWindow('XFEL Strahl', WINDOW_OFFSET[0], WINDOW_OFFSET[1])
+    cv2.moveWindow('Messobjekt', WINDOW_OFFSET[0]+IMAGE_SIZE[0], WINDOW_OFFSET[1])
+    cv2.moveWindow('Bestrahlung', WINDOW_OFFSET[0], WINDOW_OFFSET[1] + IMAGE_SIZE[1])
+    cv2.moveWindow('Streubild', WINDOW_OFFSET[0] + IMAGE_SIZE[0], WINDOW_OFFSET[1] + IMAGE_SIZE[1])
 
-    cv2.moveWindow('Control Room', 0, 0 )
-    cv2.moveWindow('Beam', WINDOW_OFFSET[0], WINDOW_OFFSET[1])
-    cv2.moveWindow('Object', WINDOW_OFFSET[0]+IMAGE_SIZE[0], WINDOW_OFFSET[1])
-    cv2.moveWindow('Illumination', WINDOW_OFFSET[0] + IMAGE_SIZE[0]//2, WINDOW_OFFSET[1] + IMAGE_SIZE[1])
-    cv2.moveWindow('Detector image', WINDOW_OFFSET[0] + 3*IMAGE_SIZE[0]//2, WINDOW_OFFSET[1] + IMAGE_SIZE[1])
+    cv2.createTrackbar('XFEL offen', 'Kontrollraum', 1, 1, nothing)
+    cv2.createTrackbar('Strahldurchmesser (hor.)','Kontrollraum',100,100,nothing)
+    cv2.createTrackbar('Strahldurchmesser (ver.)','Kontrollraum',100,100,nothing)
+    cv2.createTrackbar('Strahlstabilitaet', 'Kontrollraum', 0, 100,nothing)
+    cv2.createTrackbar('Detektor offen', 'Kontrollraum', 1, 1, nothing)
 
-    cv2.createTrackbar('Beam Shutter', 'Control Room', 0, 1, nothing)
-    cv2.createTrackbar('Horizontal beam diameter','Control Room',100,100,nothing)
-    cv2.createTrackbar('Vertical beam diameter','Control Room',100,100,nothing)
-    cv2.createTrackbar('Pointing stability', 'Control Room', 0, 100,nothing)
-    cv2.createTrackbar('Open Camera', 'Control Room', 0, 1, nothing)
+    cam = VideoCapture(camera_id)
 
     while True:
 
-        #pdb.set_trace()
-
-        hor_beam_diameter = cv2.getTrackbarPos('Horizontal beam diameter','Control Room')
-        ver_beam_diameter = cv2.getTrackbarPos('Vertical beam diameter','Control Room')
-        pointing          = cv2.getTrackbarPos('Pointing stability','Control Room')
-        camera_apt        = cv2.getTrackbarPos('Open Camera', 'Control Room')
-        shutter           = cv2.getTrackbarPos('Beam Shutter', 'Control Room')
+        hor_beam_diameter = cv2.getTrackbarPos('Strahldurchmesser (hor.)','Kontrollraum')
+        ver_beam_diameter = cv2.getTrackbarPos('Strahldurchmesser (ver.)','Kontrollraum')
+        pointing          = cv2.getTrackbarPos('Strahlstabilitaet','Kontrollraum')
+        camera_apt        = cv2.getTrackbarPos('Detektor offen', 'Kontrollraum')
+        shutter           = cv2.getTrackbarPos('XFEL offen', 'Kontrollraum')
 
 
-        img = np.zeros(shape=IMAGE_SIZE)
         if camera_apt > 0:
-            img = imread("bean.jpeg",1)
+            #img = imread("bean.jpeg",1)
             ret_val, img = cam.read()
 
         # Mirror
@@ -91,51 +90,78 @@ def show_webcam():
 
         hsv_value = hsvimg[:,:,2]
 
-        beam = np.zeros(greyimg.shape)
+        beam = np.zeros(greyimg.shape)+1.e-6
         if shutter > 0:
             beam = makeGaussian(beam.shape, hor_beam_diameter, ver_beam_diameter, pointing)
 
 
         illumination = np.multiply(beam,greyimg)
-        imgFT = np.log10(1.+np.abs(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(illumination)))))
 
-        imgFT -= imgFT.min()
-        if imgFT.max() > 0:
-            imgFT /= imgFT.max()
-
-        ## For correct display images have to be normalized
-        ## Add some background.
-        ##imgFT = 1.0*imgFT + np.random.random(size=imgFT.shape) * background/1000. * imgFT.max()
-
+        #imgFT = np.log10(1.+np.abs(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(illumination)))))
+        #imgFT -= imgFT.min()
         #if imgFT.max() > 0:
-            #imgFT = imgFT/np.max(imgFT)
-        #pdb.set_trace()
+            #imgFT /= imgFT.max()
 
-        beam_cmap_interface = ScalarMappable(norm=matplotlib.colors.Normalize(vmin=0.0, vmax=1.0), cmap='jet_r')
-        viridis_beam = beam_cmap_interface.to_rgba(beam)
+        imgFT = np.abs(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(illumination))))
 
-        imgFT_cmap_interface = ScalarMappable(norm=matplotlib.colors.Normalize(vmin=0.,vmax=1.0), cmap='jet_r')
-        xfel_imgFT = imgFT_cmap_interface.to_rgba(imgFT)
+        colormap_beam = 'viridis'
+        colormap_img = xfel
+        beam_cmap_interface = ScalarMappable(norm=matplotlib.colors.Normalize(), cmap=colormap_beam)
+        viridis_beam = rgba_to_bgra(beam_cmap_interface.to_rgba(beam, bytes=True))
+
+        imgFT_cmap_interface = ScalarMappable(norm=matplotlib.colors.LogNorm(), cmap=colormap_img)
+
+        # Convert to RGBA (uint8 based).
+        xfel_imgFT = rgba_to_bgra(imgFT_cmap_interface.to_rgba(imgFT, bytes=True))
+        # Swap rgba to bgra (because different convensions between matplotlib and cv)
 
 
-        imshow('Beam',viridis_beam)
-        imshow('Object',img)
+        imshow('XFEL Strahl',viridis_beam)
+        imshow('Messobjekt',img)
 
         hsv_value *= beam
         hsvimg[:,:,2] = hsv_value
         illuminated_img = matplotlib.colors.hsv_to_rgb(hsvimg)
 
-        imshow('Illumination',illuminated_img)
-        imshow('Detector image',xfel_imgFT)
+        imshow('Bestrahlung',illuminated_img)
+        imshow('Streubild',xfel_imgFT)
+
 
         #time.sleep(2)
-
-        if waitKey(1) == 27:
+        k = waitKey(1)
+        if k == 27:
             break  # esc to quit
+        elif k == 65377:
+            np.save('imgFT.npz', imgFT)
+            take_snapshot(data=(img, xfel_imgFT))
+
     destroyAllWindows()
 
+def take_snapshot(data):
+    # Stitch images vertically.
+    img1 = data[0]
+    img2 = data[1][:,:,:-1]
+
+    cv2.imwrite("/tmp/img1.jpg", img1)
+    cv2.imwrite("/tmp/img2.jpg", img2)
+
+    command = "convert /tmp/img1.jpg /tmp/img2.jpg -append /tmp/image.jpg && display /tmp/image.jpg"
+
+    proc = subprocess.Popen(command, shell=True)
+
+    k = waitKey()
+    if k == 27:
+        return  # esc to quit
+    elif k == 65377:
+        hcopy_cmd = "./print.sh /tmp/image.jpg"
+        hcopy_proc = subprocess.Popen(hcopy_cmd, shell=True)
+
+def rgba_to_bgra(img):
+    r,g,b,a = cv2.split(img)
+    return cv2.merge((b,g,r,a))
+
 def main():
-    show_webcam()
+    show_webcam(camera_id=1)
 
 if __name__ == '__main__':
     main()
--- exfel_colormap.py
+++ exfel_colormap.py
@@ -1,8 +1,14 @@
+#! /usr/bin/env python
 import matplotlib
+from matplotlib.cm import ScalarMappable
+#from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+import sys
 import numpy
 
-from matplotlib import pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+import cv2
+from cv2 import imread, imshow, imwrite, namedWindow, destroyAllWindows, waitKey
 
 # Define colors
 #hex_colors = ["#dadada", # light grey
@@ -17,12 +23,15 @@ from matplotlib.colors import LinearSegmentedColormap
               #]
 
 hex_colors = [
-              "#ffffff", # white
               "#e3ebf2", # 20% blue
               "#a4c3d6", # 60% blue
               "#0d1546", # night blue
-              "#a26100", # 2/3 of orange
+              #"#513000", # 1/3 of orange
+              "#794900", # 1/2 of orange
+              #"#a26100", # 2/3 of orange
               "#f39200", # orange
+              "#ffff00", # full orange
+              "#ffffff", # white
 ]
 
 hex_colors_r = [
@@ -36,18 +45,38 @@ hex_colors_r = [
 
 
 # Fewer bins will result in "coarser" colomap interpolation
-nbins = 128
+nbins = 256
 
 # Setup the colormap by linear segmentation.
 exfel_colormap = LinearSegmentedColormap.from_list( "exfel_colormap", hex_colors, N=nbins)
 exfel_colormap_r = LinearSegmentedColormap.from_list( "exfel_colormap_r", hex_colors_r, N=nbins)
 
 
-def main():
-    x,y = numpy.meshgrid(numpy.linspace(-4,4,81), numpy.linspace(-4,4,81))
-    fake_data = 8*numpy.exp(-(x**2+y**2))
-    plt.imshow(fake_data, cmap=exfel_colormap)
-    plt.show()
+def main(args):
+
+    if len(args) == 0:
+        x,y = numpy.meshgrid(numpy.linspace(-4,4,81), numpy.linspace(-4,4,81))
+        fake_data = 8*numpy.exp(-(x**2+y**2))
+        plt.imshow(fake_data, cmap=exfel_colormap)
+
+    elif len(args) == 1:
+        fake_data = numpy.load(args[0])
+        imgFT_cmap_interface = ScalarMappable(norm=matplotlib.colors.LogNorm(), cmap=exfel_colormap)
+
+        # Convert to RGBA (uint8 based).
+        xfel_imgFT = imgFT_cmap_interface.to_rgba(fake_data, bytes=True)
+
+        # Swap rgba to bgra (because different convensions between matplotlib and cv)
+        cv2_r, cv2_g, cv2_b, cv2_a = cv2.split(xfel_imgFT)
+        xfel_imgFT = cv2.merge((cv2_b, cv2_g, cv2_r, cv2_a))
+        namedWindow('Image')
+        while True:
+            imshow('Image', xfel_imgFT)
+            k = waitKey(500)
+            if k == 27:
+                break  # esc to quit
+
+        destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
